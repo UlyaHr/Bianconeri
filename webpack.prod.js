8067fee5356f017ddeca4289a6ca2039a3923a32
UlyaHr <habiburrahman.ulya@gmail.com>
2020-08-02 03:43:41 +0700

    restore webpack

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,37 +1,23 @@
-const merge = require("webpack-merge");
-const common = require("./webpack.common.js");
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const MinifyPlugin = require("babel-minify-webpack-plugin");
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: "production",
-  module: {
-    rules: [
-      /** Babel Loader */
-      {
-        test: /\.js$/,
-        exclude: "/node_modules/",
-        use: [
-          {
-            loader: "babel-loader",
-            options: {
-              presets: ["@babel/preset-env"],
-            },
-          },
-        ],
-      },
-    ],
-  },
-  optimization: {
-    minimizer: [
-      new UglifyJsPlugin({
-        test: /\.js(\?.*)?$/i,
-      })
-    ]
-  },
-  plugins:[
-    new MinifyPlugin({}, {
-      comments: false
-    })
-  ]
-});
+    mode: "production",
+    module: {
+        rules: [
+            /** Babel Loader */
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+})
